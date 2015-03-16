90970005372be66b0fa328c66b585f3e1ac4aba6
Chris Bushman <christopherwbushman@gmail.com>
2015-03-16 12:42:10 -0500

    no message

---
+++ Unity Project/Assets/Resources/Scripts/Effects/Effect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Effect : MonoBehaviour
+{
+
+	[SerializeField]
+	public string Name;
+
+	[SerializeField]
+	public bool Detrimental;
+
+	[SerializeField]
+	public bool SingleShot;
+
+	[SerializeField, Range(0,20)]
+	public int Magnitude;
+
+	/// <summary>
+	/// Duration of the effect in seconds
+	/// </summary>
+	[SerializeField, Range(1, 300)]
+	public int Duration;
+
+
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
---
+++ Unity Project/Assets/Resources/Scripts/PartyHandler.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PartyHandler : MonoBehaviour
+{
+
+	private static PartyHandler _instance;
+
+	/// <summary>
+	/// Holds the current party memebers
+	/// </summary>
+	[SerializeField]
+	private List<GameObject> PartyList;
+
+
+
+
+	/// <summary>
+	/// Creates an instance of UIInterfaceManager as a gameobject if an instance does not exist
+	/// </summary>
+	public static PartyHandler Instance
+	{
+		get { return _instance ?? (_instance = new GameObject("PartyHandler").AddComponent<PartyHandler>()); }
+	}
+
+
+	// TODO possibly add remove via string name instead of gameobject
+	public void AddPartyMember(GameObject member)
+	{
+		// Logic code to ensure that this member is allowed to be in the party
+		PartyList.Add(member);
+	}
+
+	public void RemovePartyMember(GameObject member)
+	{
+		// Logic code to ensure that this member is allowed to be in the party
+		PartyList.Remove(member);
+	}
+
+
+
+
+
+
+
+}
