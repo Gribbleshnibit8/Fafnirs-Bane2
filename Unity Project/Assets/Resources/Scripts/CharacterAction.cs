efcb0f57f8ae91f6a1b8d2630de026e71f92f090
Gribbleshnibit8 <gribbleshnibit8@gmail.com>
2015-04-17 16:09:34 -0500

    Added character actions

--- Unity Project/Assets/Resources/Scripts/CharacterAction.cs
+++ Unity Project/Assets/Resources/Scripts/CharacterAction.cs
@@ -6,41 +6,56 @@ public enum AttackDirection
 	Left, Right, Down, Up
 }
 
+//public enum ActionType
+//{
+//	Move,
+//	Attack,
+//	Inventory,
+//	Skillz,
+//	Menu,
+//	None
+//}
+
 public class CharacterAction
 {
 
 	public ActionType Action = ActionType.None;
-
 	public int Cost = 1;
+
+	public CharacterAction() { }
+
+	public CharacterAction(ActionType action, int cost)
+	{
+		Action = action;
+		Cost = cost;
+	}
+
+}
+
+public class CharacterActionMove : CharacterAction
+{
 
 	public Vector2 Position;
 
-	//public InventoryItem Item = 
+	public CharacterActionMove(ActionType action, int cost, Vector2 position)
+	{
+		Action = action;
+		Cost = cost;
+		Position = position;
+	}
+	
+}
 
+public class CharacterActionAttack : CharacterAction
+{
+	
 	public AttackDirection Direction = AttackDirection.Up;
 
-	public CharacterAction(ActionType action, int cost)
+	public CharacterActionAttack(ActionType action, int cost, AttackDirection direction)
 	{
-		
-	}
-
-	public CharacterAction(ActionType action, int cost, Vector2 position)
-	{
-
-	}
-
-	public CharacterAction(ActionType action, int cost, AttackDirection direction)
-	{
-
-	}
-
-	public CharacterAction(ActionType action, int cost)
-	{
-
-	}
-	public CharacterAction(ActionType action)
-	{
-		
+		Action = action;
+		Cost = cost;
+		Direction = direction;
 	}
 
 }
